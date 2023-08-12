7107be6f0ce5c71f1e114196c29ab67122241529
Talal <alamtalal004@gmail.com>
2023-08-12 18:44:08 -0400

    I added the status Screen

---
+++ src/Screen/Status.jsx
@@ -0,0 +1,36 @@
+import { Box, Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "@emotion/react";
+import { tokens } from "../../theme";
+import { io } from "socket.io-client";
+import StatusBox from "../Components/Common/StatusBox";
+const socket = io("https://uptime-check-backend.onrender.com");
+//const socket = io("http://localhost:3001");
+
+export default function Status() {
+  const theme = useTheme();
+  const colors = tokens(theme.pallete.mode);
+  const [uptimeChecks, setUptimeCheck] = useState();
+
+  socket.on("currentUptimeChecks", (newUptimeCheck) => {
+    setUptimeCheck(newUptimeCheck);
+  });
+  useEffect(() => {}, [uptimeChecks]);
+
+  return (
+    <Grid
+      container
+      paddingY={"30px"}
+      spacing={2}
+      backgroundColor={colors.primary[500]}
+      display={"flex"}
+      justifyContent={"center"}
+    >
+      {uptimeChecks
+        ? uptimeChecks.map((check) => (
+            <StatusBox data={check} key={check._id} />
+          ))
+        : null}
+    </Grid>
+  );
+}
