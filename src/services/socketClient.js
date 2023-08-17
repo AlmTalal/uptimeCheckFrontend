e88b21263aa1127a9ecf9f577de815f885a8c753
Talal <alamtalal004@gmail.com>
2023-08-17 11:42:07 -0400

    Changed the socket server

--- src/services/socketClient.js
+++ src/services/socketClient.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-//const socket = io("https://uptime-check-backend.onrender.com");
-const socket = io("http://localhost:3001");
+const socket = io("https://uptime-check-backend.onrender.com");
+//const socket = io("http://localhost:3001");
 export default socket;
