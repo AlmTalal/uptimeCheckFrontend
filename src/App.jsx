2b092af0310a7152fdaf6c7c199c35b3ec78c40d
Talal <alamtalal004@gmail.com>
2023-08-17 10:58:13 -0400

    Adding the auth provider and updating the app when it changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,32 @@ import Navbar from "./Components/Navbar";
 import StatusScreen from "./Screen/StatusScreen/StatusScreen";
 import { Route, Routes } from "react-router-dom";
 import LoginScreen from "./Screen/Login/LoginScreen";
+import AuthProvider from "./Contexts/AuthProvider";
+import { useAuth } from "./Contexts/AuthProvider";
+import { useEffect } from "react";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const auth = useAuth();
+
+  useEffect(() => {}, [auth]);
 
   const colors = tokens(theme.pallete.mode);
   return (
     <>
       <ColorModeContext.Provider value={colorMode}>
         <ThemeProvider theme={theme}>
-          <Box backgroundColor={colors.primary[500]} height={"100%"}>
-            <Navbar />
-            <main>
-              <Routes>
-                <Route path="/" Component={StatusScreen} />
-                <Route path="/login" Component={LoginScreen} />
-              </Routes>
-            </main>
-          </Box>
+          <AuthProvider>
+            <Box backgroundColor={colors.primary[500]} height={"100%"}>
+              <Navbar />
+              <main>
+                <Routes>
+                  <Route path="/" Component={StatusScreen} />
+                  <Route path="/login" Component={LoginScreen} />
+                </Routes>
+              </main>
+            </Box>
+          </AuthProvider>
         </ThemeProvider>
       </ColorModeContext.Provider>
     </>
